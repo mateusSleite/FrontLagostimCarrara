4ab38bf4a6180e381e6494e8c3fba87cbd87b4c4
mateusSleite <mateusleite2709@gmail.com>
2023-11-27 16:45:09 -0300

    Back&Front

---
+++ Back/DTO/UserData.cs
@@ -0,0 +1,7 @@
+namespace DTO;
+
+public class UserData
+{
+    public string Cpf { get; set; }
+    public string Senha { get; set; }
+}
---
+++ Back/Model/Usuario.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+namespace Back.Model;
+
+public partial class Usuario
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Cpf { get; set; }
+    public string Email { get; set; }
+    public DateOnly DataNsc { get; set; }
+    public string Numero { get; set; }
+    public string Senha { get; set; }
+    public string Salt { get; set; }
+
+    public int? ImagemId { get; set; }
+
+    public virtual Imagem Imagem { get; set; }
+
+    public virtual ICollection<Post> Posts { get; } = new List<Post>();
+}
